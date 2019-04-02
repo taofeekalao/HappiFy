66933f14343bf0247647da72371ebdaf05d7f16c
Tawfiq Alao <taofeekalao@gmail.com>
2019-04-02 12:22:00 +0100

    Initial commit of HappiFy

---
+++ app/src/main/java/com/happify/app/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.happify.app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 4000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(SplashActivity.this, HomeActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
